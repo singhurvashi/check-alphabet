b1505033e1072d9f05817c633dcba415557a7bd4
singhurvashi <48882063+singhurvashi@users.noreply.github.com>
2019-11-01 07:48:12 -0700

    Add files via upload

---
+++ checkalphabet.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	char ch;
+	cout<< " enter the character";
+	cin>>ch;
+	if((ch>='a'&& ch<='z')||(ch>='A'&&ch<='Z'))
+	{
+		cout<<ch<<" is  alphabet";
+		
+}
+else
+{
+	cout<<ch<<"not alphabet";
+}
+	
+}
+
+
+
+
+
+
+
+
+
+
